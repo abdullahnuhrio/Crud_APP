4f5c7f302c061fdddfc2de2123de35e753d76039
Software <Software@DESKTOP-1GBK451>
2024-12-16 18:32:32 +0500

    Update Crud

---
+++ Crud_Web/Context/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Crud_Web.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Crud_Web.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; } 
+    }
+}
---
+++ Crud_Web/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Crud_Web.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+    }
+}
